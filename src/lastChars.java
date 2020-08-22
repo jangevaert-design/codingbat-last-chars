be02890af42485cf57537ec8705abb4d73058518
Jan Gevaert <jan.gevaert@gmail.com>
2020-08-22 14:15:10 -0600

    Initial commit

---
+++ src/lastChars.java
@@ -0,0 +1,14 @@
+public class lastChars {
+  public String lastChars(String a, String b) {
+    if (a.length() == 0 && b.length() != 0) {
+      return "@" + b.substring(b.length() - 1, b.length());
+    } else if (b.length() == 0 && a.length() != 0) {
+      return (a.substring(0, 1) + "@");
+    } else if (a.length() == 0 && b.length() == 0) {
+      return "@@";
+    } else {
+      return (a.substring(0, 1) + b.substring(b.length() - 1, b.length()));
+    }
+  }
+
+}
